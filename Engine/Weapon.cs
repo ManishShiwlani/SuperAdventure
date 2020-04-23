462398359c915436bc7987bd0e015e6c3b4ef788
Manish Shiwlani <manishshiwlani@gmail.com>
2020-04-22 22:33:07 -0400

    added Weapon and Quest class

---
+++ Engine/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    class Weapon
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
+        public int MinimumDamage { get; set; }
+        public int MaxmimumDamage { get; set; }
+    }
+}
+ 
